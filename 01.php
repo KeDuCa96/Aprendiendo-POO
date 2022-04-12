71b0a9f79d2851c2008f6d947fa5102c14dfa5a6
KeDuCa96 <stevanduran96@gmail.com>
2022-04-12 00:06:04 -0500

    Métodos en clases. Abstraccion, primer piedra angular  de la POO

--- 01.php
+++ 01.php
@@ -15,7 +15,6 @@ class Producto {
 /*     public $nombre; //* Esto en POO no se le reconoce como variables sino como atributo o propiedades.
     public $precio;
     public $disponible;
-
     //* Constructor = Una función que se llama automaticamente se instancie la clase:
                                 //Soporta el tipado.
     public function __construct(string $nombre, int $precio, bool $disponible) //Por acá pasamos los datos. __construct se le conoce como metodos magicos en php y este es el mas común. En otros lenguajtes el constructor es el mismo nombre de la clase, pero en php es obligatoria esta sintaxis.
@@ -31,15 +30,23 @@ class Producto {
     {
         
     }
-}
 
+    //Creamos nuestra función, pero dentro de las clases se les conoce como Metodos. 
+    //* Metodos = son similares a las funciones solo que inician con el modificador de acceso public y se pueden crear las necesarias, pero solo se podrán con los objetos declarados dentro de la clase. 
+    public function mostrarProducto () {
+        echo "El producto es: ". $this->nombre ." Y su precio es: ". $this->precio;
+    }
+}
+//? Definir atributos y metodos se le conoce como la abstracción, es decir la forma que van a tener nuestras clases.
 
 
 //* 3. Instanciar
 $producto = new Producto('Tablet', 400, true); //siempre que vayamos a instanciar una clase debemos colocar el new.
+// Ejecutamos el metodo.
+$producto->mostrarProducto();
 
 //asignamos valores  a los atributos
-/* $producto->nombre = 'Tablet'; //* Como es un objeto se cambia la forma de acceder. Se le conoce como sintaxis flecha.
+/* $producto->nombre = 'Tablet'; // Como es un objeto se cambia la forma de acceder. Se le conoce como sintaxis flecha.
 $producto->precio = 350;
 $producto->disponible = true; */
  
@@ -48,7 +55,8 @@ var_dump($producto);
 echo "</pre>";
 
 //* Una de las ventjas de POO s que nos permite instanciar varios objetos 
-$producto2 = new Producto('Celular', 350, false); 
+$producto2 = new Producto('Celular', 350, false);
+$producto2->mostrarProducto();
 /* $producto->nombre = 'Celular'; 
 $producto->precio = 450;
 $producto->disponible = false; */
