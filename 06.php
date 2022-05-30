914cf7580dde8c2bbe487a44e8ee5feaed79da32
KeDuCa96 <stevanduran96@gmail.com>
2022-05-30 12:20:12 -0500

    Aprendiendo interfaces

--- 06.php
+++ 06.php
@@ -1,4 +1,32 @@
 <?php include 'includes/header.php';
 
+//? Interfaces : Las interfaces permiten agrupar un grupo de declaraciones de funciones, pero no las va a implementar. Son un plano de las funciones que tiene una clase. Basicamente nos dice que hace una clase, que funciones tiene, que datos tiene y retorna, pero no como lo hace.
+
+interface TransporteInterface {
+    //Aclaramos la implementación de la clase, es decir que funciones tiene y que retornan.
+    public function getinfo() : string; 
+    public function getRuedas() : int;
+        // Si en la interfaz retornamos un tipo de de dato diferente a la función, no nos va a dar error porque la interfaz solo define la forma no valida nada.
+        // Si aclaramos una función que no tiene la clase sí nos mostrará el error en la clase.
+        
+}
+
+class Transporte implements TransporteInterface{ //implements agrega la interfaz
+
+    // Podemos heredar atributos
+    public function __construct(protected int $ruedas, protected int $capacidad)
+    {
+        
+    }
+
+    //Podemos heredar metodos
+    public function getinfo() : string{
+        return "El transporte tiene ". $this->ruedas ." ruedas, una capacidad de ". $this->capacidad. " personas";
+    }
+
+    public function getRuedas() : int {
+        return $this->ruedas;
+    }
+}
 
 include 'includes/footer.php';
