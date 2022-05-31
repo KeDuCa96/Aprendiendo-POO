9bf2a7c60e4eb237c7f854c05361d2fe588acfbb
KeDuCa96 <stevanduran96@gmail.com>
2022-05-30 23:40:46 -0500

    Aprendiendo composer

--- 08.php
+++ 08.php
@@ -1,5 +1,6 @@
 <?php include 'includes/header.php';
 
+require 'vendor/autoload.php';
 //? Autoload
 //* Al tener muchos requiere nos tocaría estar pendiente de cargar una clase que dependa de otra y no la cargamos en el orden correcto.
 //? Namespace
@@ -9,19 +10,21 @@
 //Esto lo usamos para borrar el namespace desde la instancia. Esto se usa mucho en el freamwokr de Laravel.
 use App\Clientes;
 use App\Detalles;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 // Incluimos las clases
 /* require 'Class/clientes.php';
 require 'Class/detalles.php'; */
 
-
+/* 
 function mi_autoload($clase){ // Esta función debemos pasarla a la función autoload: spl_autoload_register
     $partes = explode('\\', $clase); //primer argumento lo que vamos a buscar y segundo donde lo vamos a buscar. Esto nos crea un arreglo dondee le va a dar a una posicón a App y otra a la clase.
     require __DIR__ . '/Class/' . $partes[1]. '.php'; //le damos la posición 1 ya que en ella se encuentra el nombre de la clase.
 }
 
 // Autoload
-spl_autoload_register('mi_autoload'); // A pesar que es una función va entre comillas.
+spl_autoload_register('mi_autoload'); // A pesar que es una función va entre comillas. */
 
 /* class Clientes {
     public function __construct()
